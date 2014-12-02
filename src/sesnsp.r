ab33c55ad57fd5c621ac2134734da85a32246cbc
animalito <animalsalvaje@gmail.com>
2014-12-02 00:45:18 -0600

    limpieza de dependiente

---
+++ src/sesnsp.r
@@ -0,0 +1,69 @@
+library(plyr)
+library(dplyr)
+library(tidyr)
+library(stringr)
+
+calculateRate <- function(x, rate, control.pop) {
+    if(is.numeric(x)) {
+        x <- ifelse(control.pop != 0, (rate/control.pop)*x, 0)
+    }
+    return(x)
+}
+
+setwd("~/Dropbox/maestria/maestria/src")
+data <- read.table('../data/segob_secretariadoEjecutivoIncidenciaDelictiva_municipios.csv', dec=',', header=T, sep=',',quote = "\"'", stringsAsFactors=F)
+names(data) <- tolower(names(data))
+
+#dim(data)
+
+# cataloguin de claves de estados
+estados <- read.csv('../data/catalogo_estados.csv', header=T)
+
+data <- merge(data, estados[,c('estado','merge')], by.x="entidad", by.y="merge", all.x=T, all.y=F)
+
+#dim(data)
+
+# clean up general
+data$cvegeo <- paste0(
+    substring(as.character(data$estado + 100),2,3) ,
+    substring(as.character(data$clave.municipio + 1000),2,4))
+
+data$concepto <- str_trim(tolower(data$concepto))
+data$modalidad <- str_trim(tolower(data$modalidad))
+data$tipo <- str_trim(tolower(data$tipo))
+data$subtipo <- str_trim(tolower(data$subtipo))
+
+#
+data <- subset(data, año==2013)
+# data$total <- as.numeric(data$total)
+# all
+all <- data %>%
+    group_by(cvegeo) %>%
+    summarise(todos=sum(total, na.rm=T))
+
+# homicidios dolosos modalidad=homicidios, tipo=dolosos
+homicidios <- filter(data, modalidad=="homicidios", tipo=="dolosos") %>%
+    group_by(cvegeo) %>%
+    summarise(homicidios=sum(total,na.rm=T))
+
+# robo comun modalidad = robo comun
+robos <- filter(data, subtipo=="de vehiculos") %>%
+    group_by(cvegeo) %>%
+    summarise(robos=sum(total, na.rm=T))
+
+# juntamos
+dependent <- merge(merge(all,homicidios), robos)
+
+# mergeamos con la base de results
+
+df <- read.csv('../data/base.csv', header=T)
+df$cvegeo <- substring(as.character(df$cvegeo + 100000),2,6)
+df_m <- merge(df[,c('cvegeo','admin1','admin2','inegi_cpv_pob1')], dependent, by="cvegeo", all.x=T, all.y=F)
+
+# saco tasas
+df_m <-mutate(df_m,
+              todos_r=calculateRate(todos, rate=100000, control.pop=inegi_cpv_pob1),
+              homicidios_r=calculateRate(homicidios, rate=100000, control.pop=inegi_cpv_pob1),
+              robos_r=calculateRate(robos, rate=100000, control.pop=inegi_cpv_pob1))
+
+write.csv(df_m, '../data/dependent.csv')
